7f1213fd7a8a78c1051562fb9393dcd914066c3a
F-nyttt <franksils345@gmail.com>
2023-01-17 19:20:48 -0300

    Exercício incompleto

---
+++ loops/src/NomeIdade.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NomeIdade {
+
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    String nome = sc.next();
+    int idade = sc.nextInt();
+
+
+    System.out.println("Nome: " + nome);
+
+    System.out.println("Idade: " + idade);
+
+
+
+    }
+}
