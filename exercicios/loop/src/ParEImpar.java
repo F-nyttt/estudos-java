848e5351e6980971cfffbac92973d0c489bf2b58
F-nyttt <franksils345@gmail.com>
2023-01-19 20:55:34 -0300

    Estrutura

---
+++ exercicios/loop/src/ParEImpar.java
@@ -0,0 +1,12 @@
+/**
+ * faça um programa que leia N números inteiros
+ * mostre a quantidade de números ímpares e pares
+ */
+
+public class ParEImpar {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
