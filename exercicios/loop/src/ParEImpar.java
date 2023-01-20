529a15df59a90c25345311b003de935bfa0de579
F-nyttt <franksils345@gmail.com>
2023-01-19 21:11:43 -0300

    Exercício

--- exercicios/loop/src/ParEImpar.java
+++ exercicios/loop/src/ParEImpar.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * faça um programa que leia N números inteiros
  * mostre a quantidade de números ímpares e pares
@@ -6,7 +7,31 @@
 public class ParEImpar {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Hello world!");
+        int quantNumeros;
+        int numero;
+
+        int quantPares = 0;
+        int quantimpares = 0;
+
+        System.out.println("Quantidade de números: ");
+        quantNumeros = sc.nextInt();
+
+        int contador = 0;
+
+        do {
+            System.out.println("Número: ");
+            numero = sc.nextInt();
+
+            if (numero % 2 == 0 ) quantPares++;
+            else quantimpares++;
+
+            contador += 1;
+
+        } while (contador < quantNumeros);
+
+        System.out.println("Quantidade pares : " + quantPares);
+        System.out.println("Quantidade ímpares :" + quantimpares);
     }
 }
